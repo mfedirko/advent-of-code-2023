de694fc8fb6fc53054417e7366297418adf2a986
Michael Fedirko <no@email.com>
2023-12-29 23:34:00 -0600

    Day 2 solution

---
+++ src/main/kotlin/io/mfedirko/aoc/Day2.kt
@@ -0,0 +1,47 @@
+package io.mfedirko.aoc
+
+object Day2 {
+    fun partOne(actual: Map<String, Int>): Int {
+        return InputReaderUtil.read("day2")
+            .map { parseGame(it) }
+            .filter { it.isPossible(actual) }
+            .map { it.id }
+            .sum()
+    }
+
+    fun partTwo(): Int {
+        return InputReaderUtil.read("day2")
+            .map { parseGame(it) }
+            .map { it.toPower() }
+            .sum()
+    }
+
+    private fun parseGame(line: String): Game {
+        val id: Int = line.substring("Game ".length, line.indexOf(':')).toInt()
+        val counts = line.substringAfter(": ").split(", ", "; ").groupBy(
+            { segment -> segment.split(' ')[1] },
+            { segment -> segment.split(' ')[0].toInt()}
+        )
+        return Game(id, counts)
+    }
+
+    class Game(val id: Int, counts: Map<String, List<Int>>) {
+        private val max: Map<String, Int>
+        init {
+            val maxPairs: Array<Pair<String, Int>> = counts.entries.map { e -> e.key to e.value.max() }.toTypedArray()
+            max = mapOf(*maxPairs)
+        }
+
+        fun isPossible(actual: Map<String, Int>): Boolean {
+            return max.all { e -> (actual[e.key] ?: 0) >= e.value }
+        }
+
+        fun toPower(): Int {
+            return listOf("red", "green", "blue").map { max[it] ?: 0 }.fold(1) { acc, i -> acc * i }
+        }
+    }
+}
+
+fun main() {
+    println(Day2.partTwo())
+}
